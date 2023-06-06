24cbaf7c54c4437ed3e789f546cec5350c664b29
GraemeWard24 <garaeme@hotmail.com>
2023-06-06 22:22:02 +0800

    Update number_base_converter.py

--- number_base_converter.py
+++ number_base_converter.py
@@ -17,11 +17,11 @@ Created on Tue May 9 2023
 class number_base:
     # Set value of number and base of number in input
     def __init__(self, value, base):
-        self._value = value
-        self._base = base
+        self.value = value
+        self.base = base
     
     def __str__(self):
-        return f'{self._value} in base {self._base}'
+        return f'{self.value} in base {self.base}'
     
     # Currently not working    
     # def __int__(self):
@@ -34,41 +34,60 @@ class number_base:
         total = 0 # output sum
         
         # Loop through characters, convert to decimal, then sum together and output
-        for char in self._value[::-1]: # work in reverse in increasing powers
+        for char in self.value[::-1]: # work in reverse in increasing powers
             
             # Convert numbers greater than ten to numeric if the base allows it
-            if (self._base > 10) & (char == "X"):
+            if (self.base > 10) & (char == "X"):
                 char = 10
-            if (self._base > 11) & (char == "E"):
+            if (self.base > 11) & (char == "E"):
                 char = 11
                 
             # Check if any characters > base
-            if int(char) > self._base:
-                print("char " + char + " is greater than base ", str(self._base))
+            if int(char) > self.base:
+                print("char " + char + " is greater than base ", str(self.base))
                 break
     
             # Calculate output in decimal and add to total
-            total += int(char) * self._base ** power
+            total += int(char) * self.base ** power
             power += 1 # increase power for each element
    
         # Output the total sum
         return(total)
+    
+    # Function to convert from decimal to any base
+    def from_decimal(self, base_to):
+        val = self.to_decimal() # firstly convert value to decimal
+        
+        # Determine number of digits needed for new base
+        i = 1 # iterator for number of digits required (start at power 1 as power 0 can use power 1 to solve)
+        flag = False # initialise flag as false to be made true when we find the maximum power
+        while flag == False:
+            if int(base_to ** i / val) == 0: # if the floor of base ^ i / val is 0 then we need more power
+                i += 1
+                continue
+            else:
+                flag = True # we have enough power so exit loop
+        
+        # Now we need to find the values, place in a list 'i' elements long
+        #digits = [0] * i # initialise list
+        #for j in digits:
+        #    digits[j] # get powers needed to make the number in the particular base
+        
+        
+        return(val)
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
 # Testing
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
 test = number_base("24", 12)
 
-print(test._value)
-print(test._base)
-print(test.to_decimal())
-print(test._value)
-print(test._base)
+print(test.from_decimal(9))
 
-print(test)
-#int(test)
-#str(test)
-#print(to_decimal("19", 12))
+#print(test.value)
+#print(test.base)
+#print(test.to_decimal())
+
+#print(test)
 
 # Make a from_decimal() function to any base
     # Loop through powers (increasing) of new base to figure out how many digits we need (use int function)
